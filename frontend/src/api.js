09b7b0a42097f8f974a556e1b70160a645af5b1c
gurusishya26-wq <gurusishya26@gmail.com>
2026-03-25 13:57:35 +0530

    Update API base URL to production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/courses",
+  baseURL: "https://gyani-vxc9.onrender.com/api/courses",
 });
 
 export default API;
